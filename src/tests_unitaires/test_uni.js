2c53c003181d51f152c004f4eed424161d0ebdbb
hbaltz <hugo.baltz@ensg.eu>
2016-04-10 22:52:34 +0200

    Style + correction, js id->idp

--- src/tests_unitaires/test_uni.js
+++ src/tests_unitaires/test_uni.js
@@ -10,15 +10,15 @@ function effectuer_test_unitaire(para) {
 		async: false
 	}).responseText;
 
-	// On recupere l'id :
+	// On recupere l'idp :
 	info = JSON.parse(html);
-	if (typeof info.id == undefined) {
+	if (typeof info.idp == undefined) {
 		$('#actu').html('Erreur lors de l\'initialisation').addClass('rate');
 	} else {
 		$('#actu').html('Initialisation réussie : on lance les requetes...');
-		var id = info.id;
+		var idp = info.idp;
 		var nb_total = para.requetes.length;
-		var valide = true;
+		var validpe = true;
 		var retours = JSON.parse(para.retours);
 
 		$.each(para.requetes, function(i, elm) {
@@ -26,18 +26,18 @@ function effectuer_test_unitaire(para) {
 			$('#en_cours').html(elm[1]+'...');
 
 			var html = $.ajax({
-				url: '../'+elm[0].replace('[id]', id),
+				url: '../'+elm[0].replace('[id]', idp),
 				async: false
 			}).responseText;
 
 			if (!equals(JSON.parse(html), JSON.parse(retours[i]))) {
 				$('#error').show();
 				$('#error').append('<p>'+elm[1]+'</p>');
-				valide = false;
+				validpe = false;
 			}
 		});
 
-		if (valide) {$('#actu').html('Test unitaire réussi !').addClass('reussi');}
+		if (validpe) {$('#actu').html('Test unitaire réussi !').addClass('reussi');}
 		else {$('#actu').html('Test unitaire raté !').addClass('rate');}
 
 		// Suppression de la partie dans la table :
@@ -52,10 +52,10 @@ function effectuer_test_unitaire(para) {
 		if (/succes/.test(html)) {
 			$('#sur').html('Test terminé. Suppression de la partie dans la BDD réussie.');
 			$('#en_cours').html('Tout s\'est bien déroulé.');
-			$('#avancement').hide().delay(500);
+			$('#avancement').hidpe().delay(500);
 		} else {
 			$('#sur').html('Test terminé. Suppression de la partie dans la BDD ECHOUEE !');
-			$('#en_cours').html('Le test unitaire est validé. Mais la partie n\'a pas été supprimée correctement !');
+			$('#en_cours').html('Le test unitaire est validpé. Mais la partie n\'a pas été supprimée correctement !');
 		}
 
 	}
@@ -72,15 +72,15 @@ function maj_test_unitaire(para) {
 		async: false
 	}).responseText;
 
-	// On recupere l'id :
+	// On recupere l'idp :
 	info = JSON.parse(html);
-	if (typeof info.id == undefined) {
+	if (typeof info.idp == undefined) {
 		$('#actu').html('Erreur lors de l\'initialisation').addClass('rate');
 	} else {
 		$('#actu').html('Initialisation réussie : on lance les requetes...');
-		var id = info.id;
+		var idp = info.idp;
 		var nb_total = para.requetes.length;
-		var valide = true;
+		var validpe = true;
 		var req = [];
 
 		$.each(para.requetes, function(i, elm) {
@@ -88,14 +88,14 @@ function maj_test_unitaire(para) {
 			$('#en_cours').html(elm[1]+'...');
 
 			var html = $.ajax({
-				url: '../'+elm[0].replace('[id]', id),
+				url: '../'+elm[0].replace('[idp]', idp),
 				async: false
 			}).responseText;
 
 			req.push(html);
 		});
 
-		$('#avancement').html('<textarea id="req" style="height:300px;width:800px;"></textarea>');
+		$('#avancement').html('<textarea idp="req" style="height:300px;widpth:800px;"></textarea>');
 		$('#req').val(JSON.stringify(req).replace(/\\/g, '\\\\'));
 	}
 }
